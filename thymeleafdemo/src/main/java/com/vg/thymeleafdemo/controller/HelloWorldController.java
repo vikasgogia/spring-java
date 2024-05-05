d9b91795ef443ad4f227e49c65f896651187a5f3
vikasgogia <vikasgog10@gmail.com>
2024-05-05 15:08:28 -0400

    get, post, form data

--- thymeleafdemo/src/main/java/com/vg/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/vg/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.vg.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -13,6 +17,19 @@ public class HelloWorldController {
 
     @GetMapping("/process-form")
     public String processForm() {
+        return "processform";
+    }
+
+    @GetMapping("/process-form2")
+    public String processForm2(HttpServletRequest request, Model model) {
+        String name = request.getParameter("studentName").toUpperCase();
+        model.addAttribute("sName", name);
+        return "processform";
+    }
+
+    @PostMapping("/process-form3")
+    public String processForm3(@RequestParam("studentName") String name, Model model) {
+        model.addAttribute("sName", name.toUpperCase());
         return "processform";
     }
 }
